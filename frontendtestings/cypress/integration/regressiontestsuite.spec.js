00ea3c857122d53f1798bd82aefb64b7a1b426f1
Nils Jacobson <jacobsonnils521@gmail.com>
2021-04-17 20:01:55 +0200

    Created gitignore and test project

---
+++ frontendtestings/cypress/integration/regressiontestsuite.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+// Test suite
+describe('Regression test suite', function(){
+    
+    // Test case
+    it('Perform valid login', function (){
+        cy.visit('http://localhost:3000')
+    })
+
+
+})
